8eb16a32478d6f859aa6b9dcc84cae66900dea14
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-10-31 11:44:42 +0000

    comments added for the node side

--- modules/donate.js
+++ modules/donate.js
@@ -1,6 +1,20 @@
+/**
+ * Module to manage donations.
+ */
+
+/**
+ * Collection of donations. The collections are grouped by cause, and each
+ * cause contains an array of donations (int)
+ */
 var _donations = {};
 
+/**
+ * Public module
+ */
 var donate = {
+	/**
+	 * Method to get the number of donations for a given cause
+	 */
 	getDonations: function(cause) {
 		if (_donations[cause]) {
 			return _donations[cause].length;
@@ -10,6 +24,11 @@ var donate = {
 		}
 	},
 
+	/**
+	 * Add a donation to a given cause.
+	 * A Donation must be a positive integer.
+	 * Will throw an exception if the donation is not valid.
+	 */
 	addDonation: function(cause, value) {
 		if (value != parseInt(value, 10)
 			|| value < 0) {
